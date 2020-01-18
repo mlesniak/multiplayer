78537410a173ccfa855736ef0c86da99dd762f5b
Michael Lesniak <mail@mlesniak.com>
2020-01-18 10:30:31 +0100

    Add image drawing animation

--- main.go
+++ main.go
@@ -10,10 +10,22 @@ import (
 type World struct {
 	a  uint8
 	up bool
+
+	frame int64
 }
 
 var world = World{
 	// Empty for now.
+}
+
+var gopherImage *ebiten.Image
+
+func init() {
+	var err error
+	gopherImage, _, err = ebitenutil.NewImageFromFile("asset/gopher.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
@@ -24,6 +36,7 @@ func main() {
 
 func update(screen *ebiten.Image) error {
 	// Update state.
+	world.frame++
 	if world.a == 255 || world.a == 0 {
 		world.up = !world.up
 	}
@@ -45,6 +58,14 @@ func update(screen *ebiten.Image) error {
 		A: 255,
 	})
 	ebitenutil.DebugPrint(screen, "Hello, world")
+
+	op := &ebiten.DrawImageOptions{}
+	w, h := gopherImage.Size()
+	op.GeoM.Translate(-float64(w)/2, -float64(h)/2)
+	op.GeoM.Rotate(float64(world.frame) * 0.01)
+	op.GeoM.Scale(0.2, 0.2)
+	op.GeoM.Translate(160, 100)
+	screen.DrawImage(gopherImage, op)
 
 	return nil
 }
