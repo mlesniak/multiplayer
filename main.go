a1c6bed676525c9a4930c45dba315758ea9b3088
Michael Lesniak <mail@mlesniak.com>
2020-01-19 14:00:57 +0100

    Control sprite with gamepad

--- main.go
+++ main.go
@@ -4,17 +4,18 @@ import (
 	"fmt"
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
-	"github.com/hajimehoshi/ebiten/inpututil"
 	"image/color"
 	"log"
+	"math"
 	"os"
-	"strconv"
-	"strings"
 )
 
 type World struct {
 	a  uint8
 	up bool
+
+	// Position of gopher.
+	x, y float64
 
 	frame int64
 }
@@ -35,6 +36,9 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	world.x = 160
+	world.y = 100
 }
 
 func main() {
@@ -44,48 +48,19 @@ func main() {
 }
 
 func update(screen *ebiten.Image) error {
-	// Check for gamepads.
-	for _, id := range inpututil.JustConnectedGamepadIDs() {
-		log.Printf("gamepad connected: id: %d", id)
-		gamepadIDs[id] = struct{}{}
-	}
-	for id := range gamepadIDs {
-		if inpututil.IsGamepadJustDisconnected(id) {
-			log.Printf("gamepad disconnected: id: %d", id)
-			delete(gamepadIDs, id)
-		}
-	}
-
-	ids := ebiten.GamepadIDs()
-	axes := map[int][]string{}
-	pressedButtons := map[int][]string{}
-
-	for _, id := range ids {
-		maxAxis := ebiten.GamepadAxisNum(id)
-		//log.Printf("%d/maxAxis: %d\n", id, maxAxis)
-		for a := 0; a < maxAxis; a++ {
-			v := ebiten.GamepadAxis(id, a)
-			axes[id] = append(axes[id], fmt.Sprintf("%d:%0.2f", a, v))
-		}
-		maxButton := ebiten.GamepadButton(ebiten.GamepadButtonNum(id))
-		for b := ebiten.GamepadButton(id); b < maxButton; b++ {
-			if ebiten.IsGamepadButtonPressed(id, b) {
-				pressedButtons[id] = append(pressedButtons[id], strconv.Itoa(int(b)))
-			}
-
-			// Log button events.
-			if inpututil.IsGamepadButtonJustPressed(id, b) {
-				log.Printf("button pressed: id: %d, button: %d", id, b)
-			}
-			if inpututil.IsGamepadButtonJustReleased(id, b) {
-				log.Printf("button released: id: %d, button: %d", id, b)
-			}
-		}
-	}
-
 	// Check for key presses.
 	if ebiten.IsKeyPressed(ebiten.KeyEscape) || ebiten.IsKeyPressed(ebiten.KeyQ) {
 		os.Exit(0)
+	}
+
+	// Check for gamepad movement.
+	hv := ebiten.GamepadAxis(0, 0)
+	vv := ebiten.GamepadAxis(0, 1)
+	if math.Abs(hv) > 0.10 {
+		world.x += hv
+	}
+	if math.Abs(vv) > 0.10 {
+		world.y += vv
 	}
 
 	// Update state.
@@ -110,36 +85,16 @@ func update(screen *ebiten.Image) error {
 		B: 0,
 		A: 255,
 	})
-	ebitenutil.DebugPrint(screen, "Hello, world")
 
 	op := &ebiten.DrawImageOptions{}
 	w, h := gopherImage.Size()
 	op.GeoM.Translate(-float64(w)/2, -float64(h)/2)
 	op.GeoM.Rotate(float64(world.frame) * 0.01)
 	op.GeoM.Scale(0.2, 0.2)
-	op.GeoM.Translate(160, 100)
+	op.GeoM.Translate(world.x, world.y)
 	screen.DrawImage(gopherImage, op)
 
-	str := ""
-	if len(ids) > 0 {
-		for _, id := range ids {
-			str += fmt.Sprintf("Gamepad (ID: %d):\n", id)
-			str += fmt.Sprintf("  Axes:    %s\n", strings.Join(axes[id], ", "))
-			str += fmt.Sprintf("  Buttons: %s\n", strings.Join(pressedButtons[id], ", "))
-			str += "\n"
-		}
-	} else {
-		str = "Please connect your gamepad."
-	}
-	ebitenutil.DebugPrintAt(screen, str, 0, 12)
-
-	as := []float64{
-		ebiten.GamepadAxis(0, 0), // 0：The horizontal value of the left axis
-		ebiten.GamepadAxis(0, 1), // 1：The vertical value of the left axis
-		ebiten.GamepadAxis(0, 2), // 2：The horizontal value of the right axis
-		ebiten.GamepadAxis(0, 3)} // 3：The vertical value of the right axis
-	ebitenutil.DebugPrint(screen, fmt.Sprintf("0: %0.6f, 1: %0.6f", as[0], as[1]))
-	ebitenutil.DebugPrint(screen, fmt.Sprintf("\n2: %0.6f, 3: %0.6f", as[2], as[3]))
+	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("0: %0.6f, 1: %0.6f", hv, vv), 0, 12)
 
 	return nil
 }
