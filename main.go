61795564c5178c84c3bd1562e9b20cdd08033aea
Michael Lesniak <mail@mlesniak.com>
2020-01-19 13:33:28 +0100

    Read and show axis and button values

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
+	"github.com/hajimehoshi/ebiten/inpututil"
 	"image/color"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 type World struct {
@@ -21,6 +25,10 @@ var world = World{
 
 var gopherImage *ebiten.Image
 
+var (
+	gamepadIDs = map[int]struct{}{}
+)
+
 func init() {
 	var err error
 	gopherImage, _, err = ebitenutil.NewImageFromFile("asset/gopher.png", ebiten.FilterDefault)
@@ -30,12 +38,51 @@ func init() {
 }
 
 func main() {
-	if err := ebiten.Run(update, 320, 200, 2, "Hello, world!"); err != nil {
+	if err := ebiten.Run(update, 320, 200, 3, "Hello, world!"); err != nil {
 		log.Fatal(err)
 	}
 }
 
 func update(screen *ebiten.Image) error {
+	// Check for gamepads.
+	for _, id := range inpututil.JustConnectedGamepadIDs() {
+		log.Printf("gamepad connected: id: %d", id)
+		gamepadIDs[id] = struct{}{}
+	}
+	for id := range gamepadIDs {
+		if inpututil.IsGamepadJustDisconnected(id) {
+			log.Printf("gamepad disconnected: id: %d", id)
+			delete(gamepadIDs, id)
+		}
+	}
+
+	ids := ebiten.GamepadIDs()
+	axes := map[int][]string{}
+	pressedButtons := map[int][]string{}
+
+	for _, id := range ids {
+		maxAxis := ebiten.GamepadAxisNum(id)
+		//log.Printf("%d/maxAxis: %d\n", id, maxAxis)
+		for a := 0; a < maxAxis; a++ {
+			v := ebiten.GamepadAxis(id, a)
+			axes[id] = append(axes[id], fmt.Sprintf("%d:%0.2f", a, v))
+		}
+		maxButton := ebiten.GamepadButton(ebiten.GamepadButtonNum(id))
+		for b := ebiten.GamepadButton(id); b < maxButton; b++ {
+			if ebiten.IsGamepadButtonPressed(id, b) {
+				pressedButtons[id] = append(pressedButtons[id], strconv.Itoa(int(b)))
+			}
+
+			// Log button events.
+			if inpututil.IsGamepadButtonJustPressed(id, b) {
+				log.Printf("button pressed: id: %d, button: %d", id, b)
+			}
+			if inpututil.IsGamepadButtonJustReleased(id, b) {
+				log.Printf("button released: id: %d, button: %d", id, b)
+			}
+		}
+	}
+
 	// Check for key presses.
 	if ebiten.IsKeyPressed(ebiten.KeyEscape) || ebiten.IsKeyPressed(ebiten.KeyQ) {
 		os.Exit(0)
@@ -72,6 +119,27 @@ func update(screen *ebiten.Image) error {
 	op.GeoM.Scale(0.2, 0.2)
 	op.GeoM.Translate(160, 100)
 	screen.DrawImage(gopherImage, op)
+
+	str := ""
+	if len(ids) > 0 {
+		for _, id := range ids {
+			str += fmt.Sprintf("Gamepad (ID: %d):\n", id)
+			str += fmt.Sprintf("  Axes:    %s\n", strings.Join(axes[id], ", "))
+			str += fmt.Sprintf("  Buttons: %s\n", strings.Join(pressedButtons[id], ", "))
+			str += "\n"
+		}
+	} else {
+		str = "Please connect your gamepad."
+	}
+	ebitenutil.DebugPrintAt(screen, str, 0, 12)
+
+	as := []float64{
+		ebiten.GamepadAxis(0, 0), // 0：The horizontal value of the left axis
+		ebiten.GamepadAxis(0, 1), // 1：The vertical value of the left axis
+		ebiten.GamepadAxis(0, 2), // 2：The horizontal value of the right axis
+		ebiten.GamepadAxis(0, 3)} // 3：The vertical value of the right axis
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("0: %0.6f, 1: %0.6f", as[0], as[1]))
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("\n2: %0.6f, 3: %0.6f", as[2], as[3]))
 
 	return nil
 }
